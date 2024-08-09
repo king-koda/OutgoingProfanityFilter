b30d327580435230241d1cae5c7a064adc969f04
Dz Nutz <krisimus@gmail.com>
2024-08-09 19:03:12 +0800

    - button faulty, need to make it load respective to the state

--- utils/ToggleShouldReplaceByExactWordMatch.lua
+++ utils/ToggleShouldReplaceByExactWordMatch.lua
@@ -3,4 +3,9 @@ local function ToggleShouldReplaceByExactWordMatch()
         not OPFData["shouldReplaceByExactWordMatch"]
 end
 
+local function GetExactMatchButtonState()
+    return OPFData["shouldReplaceByExactWordMatch"]
+end
+
 OPF.ToggleExactMatching = ToggleShouldReplaceByExactWordMatch
+OPF.GetExactMatchButtonState = GetExactMatchButtonState
