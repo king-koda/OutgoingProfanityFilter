7588f7520886e98ff0dc43ac175178a2a8da22c1
Dz Nutz <krisimus@gmail.com>
2024-08-12 12:22:54 +0800

    - color coding from highlight/ search was polluting the word list before being persisted, now manually cleaning it before saving it

--- utils/Save.lua
+++ utils/Save.lua
@@ -15,6 +15,12 @@ local function WordsToReplaceSave()
     local seen = {}
 
     local text = textArea:GetText()
+
+    -- Pattern to match the color-coded text
+    local colorCodePattern = "|cff0000ff(.-)|r"
+    -- remove the color coding artifacts from search/highlighting before parsing the text
+    text = string.gsub(text, colorCodePattern, "%1")
+
     -- Remove newlines
     text = string.gsub(text, "\n", "")
     -- Remove spaces
